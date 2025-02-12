d33caaaa86e64486cfcd5c7dae5084d149c9bcfe
zaviraa <541221216@student.smktelkom-pwt.sch.id>
2025-02-12 10:07:49 +0700

    add models user

--- member/models.py
+++ member/models.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import AbstractUser , Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-
-class User(AbstractUser):
-    pass
+class User(AbstractUser ):
+    groups = models.ManyToManyField(
+        Group,
+        related_name='member_user_set',  
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='member_user_permissions_set', 
+        blank=True,
+    )

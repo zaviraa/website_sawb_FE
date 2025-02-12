8b859cc8369666845ed5bdd8f49b2aec828c6ae1
zaviraa <541221216@student.smktelkom-pwt.sch.id>
2025-02-12 10:11:14 +0700

    add views.index on views.py

--- website_sawb/urls.py
+++ website_sawb/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("", views.index),
+
 ]
